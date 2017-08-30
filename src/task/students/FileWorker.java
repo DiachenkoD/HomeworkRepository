8545759d6270680c84ae1e0a335b8201e002480d
DiachenkoD <dimaecio1@gmail.com>
2017-08-30 22:14:44 +0300

    files commit

---
+++ src/task/students/FileWorker.java
@@ -0,0 +1,59 @@
+package task.students;
+
+import java.io.*;
+
+public class FileWorker {
+    public static void write(String fileName, String text) {
+        //Определяем файл
+        File file = new File(fileName);
+
+        try {
+            //проверяем, что если файл не существует то создаем его
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            //PrintWriter обеспечит возможности записи в файл
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+
+            try {
+                //Записываем текст у файл
+                out.print(text);
+            } finally {
+                //После чего мы должны закрыть файл
+                //Иначе файл не запишется
+                out.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String read(String fileName) throws FileNotFoundException {
+        //Этот спец. объект для построения строки
+        StringBuilder sb = new StringBuilder();
+
+        //exists(fileName);
+        File file = new File(fileName);
+
+        try {
+            //Объект для чтения файла в буфер
+            BufferedReader in = new BufferedReader(new FileReader( file.getAbsoluteFile()));
+            try {
+                //В цикле построчно считываем файл
+                String s;
+                while ((s = in.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            } finally {
+                //Также не забываем закрыть файл
+                in.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Возвращаем полученный текст с файла
+        return sb.toString();
+    }
+}
--- src/task/students/MainHomeworkClass.java
+++ src/task/students/MainHomeworkClass.java
@@ -1,21 +1,77 @@
 package task.students;
 
+
+import java.io.FileNotFoundException;
+
+
 public class MainHomeworkClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Person[] allPerson = new Person[4];// создаю массив объектов типа персон
 
-        allPerson[0] = new Person("Coach Bob",27,"M");
-        allPerson[1] = new Student("Lynne Brook",16,"F","HS95129",3.5);
-        allPerson[2] = new Teacher("Duke Java",34,"M",5000.0d,"Computers Science");
+        allPerson[0] = new Person("Coach Bob", 27, "M");
+        allPerson[1] = new Student("Lynne Brook", 16, "F", "HS95129", 3.5);
+        allPerson[2] = new Teacher("Duke Java", 34, "M", 5000.0d, "Computers Science");
         allPerson[3] = new CollegeStudent("Ima Frosh", 18, "F", "UCB123", 4.0d, 1, "English");
 
         allPerson[0].toString();
         allPerson[1].toString();
         allPerson[2].toString();
         allPerson[3].toString();
+        String name;
+        int age;
+        String gender;
+        String idNumber;
+        double gpa;
+        int year;
+        String major;
+
+        String informatioString = FileWorker.read("sobaka.txt");
+        String[] koma = new String[7];
+        koma = informatioString.split(",");
+        String[] s = new String[2];
+        for (int i = 0; i< koma.length; i++){
+
+            s = koma[i].split(":");
+            switch(s[0]){
+                case "name":{
+                    name = s[1];
+                    break;
+                }
+                case "age":{
+                    age = Integer.parseInt(s[1]);
+                    break;
+                }
+                case "gender":{
+                    gender = s[1];
+                    break;
+                }
+                case "student id":{
+                    idNumber = s[1];
+                    break;
+                }
+                case "gpa":{
+                    gpa = Double.parseDouble(s[1]);
+                    break;
+                }
+                case "year":{
+                    year = Integer.parseInt(s[1]);
+                    break;
+                }
+                case "major":{
+                    major = s[1];
+                    break;
+                }
+            }
+
+        }
+
 
     }
+    CollegeStudent student = new CollegeStudent(name,age,gender,idNumber,gpa,year,major);
+
+
+
 
 }
